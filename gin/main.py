be138e969aab242f2b57d0fc1e4b0300af761327
Modez MARTIN <momartin@e2r6p18.42.fr>
2017-03-30 19:59:52 +0200

    f

--- gin/main.py
+++ gin/main.py
@@ -11,6 +11,7 @@ from Quartz.CoreGraphics import kCGEventLeftMouseUp
 from Quartz.CoreGraphics import kCGMouseButtonLeft
 from Quartz.CoreGraphics import kCGHIDEventTap
 import json
+from time import sleep
 
 
 def mouseEvent(type, posx, posy):
@@ -25,8 +26,13 @@ def mousemove(posx,posy):
         mouseEvent(kCGEventMouseMoved, posx,posy);
 
 def mouseclick(posx,posy):
-        #mouseEvent(kCGEventMouseMoved, posx,posy);
+        mouseEvent(kCGEventMouseMoved, posx,posy);
         mouseEvent(kCGEventLeftMouseDown, posx,posy);
+        mouseEvent(kCGEventLeftMouseDown, posx,posy);
+        mouseEvent(kCGEventLeftMouseDown, posx,posy);
+        sleep(0.1)
+        mouseEvent(kCGEventLeftMouseUp, posx,posy);
+        mouseEvent(kCGEventLeftMouseUp, posx,posy);
         mouseEvent(kCGEventLeftMouseUp, posx,posy);
 
 class ScreenPixel(object):
@@ -54,17 +60,83 @@ class Tracker:
 	t = ""
 	ref_cards_r = ""
 	ref_cards_b = ""
+	lost_cards = []
+	op_cards = []
+	refused_op_cards = []
+	new = ""
 	def __init__(self):
 		self.loads_cards()
 		self.t = ScreenPixel()
 		self.get_table_pos()
 		self.play()
 	
+	def click_card(self,x,y):
+		x += -1
+		y += -1
+		x = x * 32 + 123
+		y = y * 37 + 322
+		mouseclick(688, 37)
+		sleep(0.2)
+		mouseclick(x + self.table_pos[0], y + self.table_pos[1])
+		sleep(0.2)
+		mouseclick(2312, 30)
+
+	def take_or_not(self):
+		res = raw_input("take ? [y-n] $>")
+		if res == "y":
+			mouseclick(688, 37)
+			sleep(0.2)
+			mouseclick(self.table_pos[0] +54, self.table_pos[1] + 185)
+			sleep(0.2)
+			mouseclick(2312, 30)
+			return True
+		else:
+			mouseclick(688, 37)
+			sleep(0.2)
+			mouseclick(self.table_pos[0] + 647, self.table_pos[1] + 216)
+			sleep(0.2)
+			mouseclick(2312, 30)
+			return False
+
+	def action(self):
+		move = raw_input("your move [x y] $>")
+		move = move.replace("j", "11").replace("q", "12").replace("k", "13").replace("t", "10")
+		move = move.replace("J", "11").replace("Q", "12").replace("K", "13").replace("T", "10")
+		move = move.split(" ")
+		self.click_card(int(move[0]), int(move[1]))
+		return move
+
+	def get_which_new_cards(self, anc):
+		for y in range(4):
+			for x in range(13):
+				if self.cards[y][x] == 1 or self.cards[y][x] == 6:
+					if anc[y][x] != self.cards[y][x]:
+						self.new = [x,y]
+						return
 
 	def play(self):
-		self.get_all_cards_pos()
-		print self.get_card()
-		self.printit()
+		while True:
+			self.get_all_cards_pos()
+			actc =  self.get_card()
+			self.cards[actc[1]][actc[0]-1] = 3
+			self.printit()
+			if not self.take_or_not():
+				self.lost_cards.append(actc)
+			else:
+				self.refused_op_cards.append(actc)
+			anc = self.cards
+			self.get_all_cards_pos()
+			self.get_which_new_cards(anc)
+			self.cards[ self.new[1] ][ self.new[0] ] = 7
+			self.printit()
+			self.cards[ self.new[1] ][ self.new[0] ] = 1
+			move = self.action()
+			move[1] = str(int(move[1])-1)
+			res = self.op_loop()
+			if res:
+				self.op_cards.append(move)
+			else:
+				self.lost_cards.append(move)
 
 	def loads_cards(self):
 		self.ref_cards_r = []
@@ -77,8 +149,8 @@ class Tracker:
 				self.ref_cards_b.append(json.loads(f.read()))
 
 	def find_color(self, r,g,b):
-		for y in range(151, 400):
-			for  x in range(121, 400):
+		for y in range(137, 151):
+			for  x in range(25, 36):
 				px = self.get_pixel(x, y)
 				if px == (40, 120, 64):
 					return (x,y)
@@ -110,15 +182,42 @@ class Tracker:
 			actx = 119
 			acty += 37
 			self.cards.append(zouze)
+		for ca in self.op_cards:
+			self.cards[int(ca[1])][int(ca[0])-1] = 5
+		for ca in self.lost_cards:
+			self.cards[int(ca[1])][int(ca[0])-1] = 4
+		for ca in self.refused_op_cards:
+			self.cards[int(ca[1])][int(ca[0])-1] = 6
 
 	def printit(self):
-		final = ""
+		final = '\033[0m'
+		final += "   1 2 3 4 5 6 7 8 9 T J Q K\n"
+		final += "   -------------------------\n"
 		for y in range(4):
+			final += '\033[0m'
+			final += str(y + 1) + "| "
 			for x in range(13):
 				if self.cards[y][x] == 1:
+					final += '\033[92m'
 					final += "X "	
-				else:
+				if self.cards[y][x] == 6:
+					final += '\033[92m'
+					final += "0 "	
+				elif self.cards[y][x] == 2:
+					final += '\033[94m'
 					final += ". "
+				elif self.cards[y][x] == 3:
+					final += '\033[95m'
+					final += "# "
+				elif self.cards[y][x] == 4:
+					final += '\033[0m'
+					final += "0 "
+				elif self.cards[y][x] == 5:
+					final += '\033[91m'
+					final += "$ "
+				elif self.cards[y][x] == 7:
+					final += '\033[95m'
+					final += "X "	
 			final += "\n"
 		print final
 
@@ -127,14 +226,13 @@ class Tracker:
 		if px == (209, 40, 24):#red
 			px = self.get_pixel(76 + self.table_pos[0], 204 + self.table_pos[1])
 			if px == (209, 40, 24):
-				return "coeur"
-			return "carreau"
+				return 0
+			return 2
 		elif px == (17, 17, 17):
 			px = self.get_pixel(68 + self.table_pos[0], 200 + self.table_pos[1])
 			if px == (17, 17, 17):
-				return "trefle"
-			return "pique"
-
+				return 1
+			return 3
 
 	def get_card_print(self):
 		arr = []
@@ -153,7 +251,6 @@ class Tracker:
 		pr = self.get_card_print()
 		while self.get_pixel(260 + self.table_pos[0], 47 + self.table_pos[1]) != (255, 255, 255):
 			pass
-		pr2 = self.get_card_print()
 		symb2 = self.get_symbole()
 		if symb2 != symb:
 			took = True
@@ -168,17 +265,17 @@ class Tracker:
 	def get_card(self):
 		symb = self.get_symbole()
 		ar = self.get_card_print()
-		if symb == "coeur" or symb == "carreau":
+		if symb == 0 or symb == 2:
 			i = 1
 			for c in self.ref_cards_r:
 				if ar == c:
-					return (i, symb)
+					return [i, symb]
 				i += 1
 		else:
 			i = 1
 			for c in self.ref_cards_b:
 				if ar == c:
-					return (i, symb)
+					return [i, symb]
 				i += 1
 
 	def save_chien(self, name):
@@ -187,81 +284,3 @@ class Tracker:
 			f.write(json.dumps(ar))
 
 tr = Tracker()
-
-
-
-
-'''
-
-
-
- xstart 119
- ystart 319 
-
-ecart y 37 
-ecart x 32
-
-247 279
-
-corner carte du bas 161 196 171
-
-119
-393
-----
-183
-319
-----
-209
-393
-----
-247
-319
-----
-279
-356
-----
-279
-393
-----
-337
-319
-----
-369
-356
-----
-369
-393
-----
-375
-319
-----
-375
-356
-----
-375
-393
-----
-391
-129
-----
-391
-136
-----
-465
-319
-----
-465
-356
-----
-465
-393
-----
-503
-319
-----
-593
-319
-
-'''
-#green back 287840 40 120 64
-#carde corner 182 209 190
