d2e69ee1ab79105072430bed542d9c7c7b60ddda
dezmou <incredibledez@gmail.com>
2017-04-18 19:06:11 +0200

    Update main.py

--- cheat/main.py
+++ cheat/main.py
@@ -1,3 +1,16 @@
+# Anti deadline script by momartin
+# Ce script existe pour sensibiliser les etudiants a la facilite de tricherie
+# en correction .
+
+# lancez "python lescript.py &"
+# le script va attendre qu'un git clone se produise dans le dossier folder
+# et remplacer le contenus du clone par le dossier source
+# votre correcteur n'y vera que du feu surtout si votre dossier source contient un .git valide
+# reglez juste ces deux variable:
+
+source = "/Users/momartin/ref" #la ou se trouve vos fichier
+folder = "/tmp" #la ou le correcteur va git clone
+
 import sys
 from os import listdir
 import os
@@ -5,30 +18,24 @@ import shutil
 from time import sleep
 import psutil
 
-source = "/source"
-folder = "/test"
 
 while True:
 	anc = listdir(folder)
 	while True:
-		print "cherche nouveau dossier"
 		if anc != listdir(folder):
 			break
 	try:
 		while True:
 			ok = True
-			print "dossier trouve"
 			for proc in psutil.process_iter():
 				if proc.name() == "git":
 					ok = False
 			if ok:
 				break
-		print "git finis"
 		neo = set(anc).symmetric_difference(listdir(folder))
-		for root, dirs, files in os.walk(source):
-		   for file in files:
-			  path_file = os.path.join(root,file)
-			  shutil.copy2(path_file,folder+"/"+list(neo)[0])
-		break
+		os.system("rm -rf "+folder+"/"+list(neo)[0] + " > /dev/null")
+		os.system("cp -R "+source+" "+folder+"/"+list(neo)[0] + " > /dev/null")
 	except:
+		print "issue"
 		continue
+
